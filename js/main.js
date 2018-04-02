a329522ae525f6590d6efee134bede086734a02e
JinCheng <595580375@qq.com>
2018-04-02 23:34:27 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 var canvas = document.getElementById('canvas')
 var ctx = canvas.getContext('2d')
-var mouseDown = false
-var startPoint = {
+var pen = document.getElementById('pen')
+var eraser = document.getElementById('eraser')
+var mouseDown = false  // 鼠标是否按下
+var isEraser = false  // 是否是橡皮擦模式
+var startPoint = {  // 两点连线的初始坐标
     x: undefined,
     y: undefined
+}
+
+pen.onclick = function() {
+    isEraser = false
+    this.disabled = true
+    eraser.disabled = false
+}
+eraser.onclick = function() {
+    isEraser = true
+    this.disabled = true
+    pen.disabled = false
 }
 
 resizeCanvas()
@@ -17,9 +31,12 @@ canvas.onmousedown = function(e) {
     mouseDown = true
     var x = e.clientX
     var y = e.clientY
-    startPoint.x = x
-    startPoint.y = y
-    drawArc(x, y, 2, 0, Math.PI*2)
+    updateStartPoint(x, y)
+    if(isEraser) {
+        ctx.clearRect(x-15, y-15, 30, 30)
+    } else {
+        drawArc(x, y, 2, 0, Math.PI*2)
+    }
 }
 
 // 鼠标移动
@@ -27,14 +44,17 @@ canvas.onmousemove = function(e) {
     if(mouseDown) {
         var x = e.clientX
         var y = e.clientY
-        drwaLine(startPoint.x, startPoint.y, x, y)
-        startPoint.x = x
-        startPoint.y = y
+        if(isEraser) {
+            ctx.clearRect(x-15, y-15, 30, 30)
+        } else {
+            drwaLine(startPoint.x, startPoint.y, x, y)
+            updateStartPoint(x, y)
+        }
     }
 }
 
 // 鼠标松开
-canvas.onmouseup = function(e) {
+canvas.onmouseup = function() {
     mouseDown = false
 }
 
@@ -43,6 +63,11 @@ function resizeCanvas() {
     var clientHeight = document.documentElement.clientHeight
     canvas.width = clientWidth
     canvas.height = clientHeight
+}
+
+function updateStartPoint(newX, newY) {
+    startPoint.x = newX
+    startPoint.y = newY
 }
 
 function drawArc(x, y, r, start, end) {
